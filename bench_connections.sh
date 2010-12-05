b9b319013aa722b743ff199c697b588e35623d2a
Aemon Cannon <aemoncannon@gmail.com>
2010-12-04 20:14:14 -0800

    Add latest benches

--- bench_connections.sh
+++ bench_connections.sh
@@ -1,10 +1,10 @@
 SERVER=192.168.2.11
 
-#for i in 1
-#do
-#    echo "Running test with message size $i"
-#    sbt "run junction://${SERVER}/testsession#xmpp 100 ${i} data/result_xmpp_connections_${1}.out"
-#done
+for i in 1
+do
+    echo "Running test with message size $i"
+    sbt "run junction://${SERVER}/testsession#xmpp 100 ${i} data/result_xmpp_connections_${1}.out"
+done
 
 #for i in 1
 #do
@@ -18,9 +18,9 @@ SERVER=192.168.2.11
 #    sbt "run junction://${SERVER}/testsession#jx 100 ${i} data/result_jx_connections_${1}.out"
 #done
 
-for i in 1
-do
-    echo "Running test with message size $i"
-    sbt "run junction://192.168.2.15/testsession#jx 100 ${i} data/result_jx_mobile_connections_${1}.out"
-done
+#for i in 1
+#do
+#    echo "Running test with message size $i"
+#    sbt "run junction://192.168.2.15/testsession#jx 100 ${i} data/result_jx_mobile_connections_${1}.out"
+#done
 
