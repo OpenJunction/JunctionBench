192a4d6589f17fa86f58727b19535c1d2d3ae92c
Aemon Cannon <aemoncannon@gmail.com>
2010-12-04 22:35:17 -0800

    Add latest data

--- bench_connections.sh
+++ bench_connections.sh
@@ -12,15 +12,15 @@ SERVER=192.168.2.11
 #    sbt "run junction://${SERVER}:6667/testsession#irc 100 ${i} data/result_irc_connections_${1}.out"
 #done
 
-for i in 1
-do
-    echo "Running test with message size $i"
-    sbt "run junction://${SERVER}/testsession#jx 100 ${i} data/result_jx_connections_${1}.out"
-done
-
 #for i in 1
 #do
 #    echo "Running test with message size $i"
-#    sbt "run junction://192.168.2.15/testsession#jx 100 ${i} data/result_jx_mobile_connections_${1}.out"
+#    sbt "run junction://${SERVER}/testsession#jx 100 ${i} data/result_jx_connections_${1}.out"
 #done
+
+for i in 1
+do
+    echo "Running test with message size $i"
+    sbt "run junction://192.168.2.15/testsession#jx 100 ${i} data/result_jx_mobile_connections_${1}.out"
+done
 
